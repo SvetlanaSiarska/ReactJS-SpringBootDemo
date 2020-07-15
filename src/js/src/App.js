66312e12446e76852e27d184815990f42cc27497
Svetlana Siarska <siarska@gmail.com>
2020-07-15 16:29:15 +0200

    first commit

---
+++ src/js/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1>Hello WOrld Sping BOOT & React!
+    </h1>
+  );
+}
+
+export default App;
